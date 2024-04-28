7816f640534978dd9ef1569cc03fb6ff0c7d2e36
Darrehan <darrehanrasool@gmail.com>
2024-04-28 12:00:15 +0530

    Dar Rehan Rasool
    sycomusiclover

--- notes.jsx
+++ notes.jsx
@@ -3,7 +3,7 @@ const notes = [
       key: 1,
       title: "Delegation",
       content:
-        "Q. How many programmers does it take to change a light bulb 🫡 `? A. None - It’s a hardware problem"
+        "Q. How many programmers does it take to change a light bulb  `? A. None - It’s a hardware problem"
     },
     {
       key: 2,
